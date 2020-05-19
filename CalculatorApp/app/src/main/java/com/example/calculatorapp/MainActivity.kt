36ee0cc40f30d019d83b73cab34c63584ede1e06
Ketan <ketantoo@gmail.com>
2020-05-19 11:36:39 +0530

    added decimal and clear functionality

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,14 +3,36 @@ package com.example.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun digitClick(view: View) {}
+    fun digitClick(view: View) {
+        tvSolution.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View){
+        tvSolution.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
     fun equalClick(view: View) {}
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvSolution.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
 }
