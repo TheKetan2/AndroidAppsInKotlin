68e57c4a9f3f44ee8f3bd71baa2c3d8d1ec002db
Ketan <ketantoo@gmail.com>
2020-05-22 08:00:26 +0530

    QuestionActivity UI finished

--- QuizApp/app/src/main/java/com/example/quizapp/QuizQuenstionActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizQuenstionActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_quenstion.*
 
 
 class QuizQuenstionActivity : AppCompatActivity() {
@@ -13,5 +14,16 @@ class QuizQuenstionActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestion()
 
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition-1]
+        progressbar.progress = currentPosition
+        tv_progress.text = "$currentPosition"+"/"+progressbar.max
+        tv_questionId.text = question!!.question
+        ivFlag.setImageResource(question.image)
+
+        tv_option_one.setText(question.optionOne)
+        tv_option_two.setText(question.optionTwo)
+        tv_option_three.setText(question.optionThree)
+        tv_option_four.setText(question.optionFour)
     }
 }
