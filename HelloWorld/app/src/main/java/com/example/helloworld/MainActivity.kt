e311f1485772825753ea24a070dec4081dd53056
Ketan <ketantoo@gmail.com>
2020-06-04 19:27:25 +0530

    dice assignment finished

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,36 +3,54 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.btn_roll)
-        val countUpButton: Button = findViewById(R.id.btn_countUp)
-        countUpButton.setOnClickListener{countUp()}
+
+
+        diceImage1 = findViewById(R.id.image_dice1)
+        diceImage2 = findViewById(R.id.image_dice2)
+
         rollButton.setOnClickListener{rollDice()}
 
     }
 
-    private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.tvRollLabel)
+    private fun randomImage(): Int{
         val randomInt = (0..6).random()
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        return drawableResource
+    }
+
+
+    private fun rollDice(){
+
+
+        diceImage1.setImageResource(randomImage())
+        diceImage2.setImageResource(randomImage())
         Toast.makeText(
             this,
             "button clicked",
             Toast.LENGTH_SHORT).show()
     }
 
-    private fun countUp(){
-        val resultText: TextView = findViewById(R.id.tvRollLabel)
-        val countUpInt = resultText.text.toString().toInt() +1
-        resultText.text = countUpInt.toString()
-    }
+
     
 
 }
