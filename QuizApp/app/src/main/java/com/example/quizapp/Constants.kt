cb13beba76b6a37ee5edd1890aca4c81b7b2a0c0
Ketan <ketantoo@gmail.com>
2020-06-07 12:10:03 +0530

    snapshot added

--- QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
@@ -10,50 +10,50 @@ object Constants{
 
 
         val que1 = Question(
-            1,"What country does this flag belong to?",
-            R.drawable.ic_flag_of_argentina,
-             "Argentina", "Australia",
-            "Armenia", "Austria",  1
+            1,"What is the name of this animal?",
+            R.drawable.beaver,
+             "Beaver", "Cat",
+            "Tiger", "Cow",  1
         )
 
         questionsList.add(que1)
 
         // 2
         val que2 = Question(
-            2, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_australia,
-            "Angola", "Austria",
-            "Australia", "Armenia", 3
+            2, "What is the name of this animal?",
+            R.drawable.bison,
+            "Pig", "Beaver",
+            "Bison", "Bear", 3
         )
 
         questionsList.add(que2)
 
         // 3
         val que3 = Question(
-            3, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Belarus", "Belize",
-            "Brunei", "Brazil", 4
+            3, "What is the name of this animal?",
+            R.drawable.boar,
+            "Bull", "Monkey",
+            "Goat", "Boar", 4
         )
 
         questionsList.add(que3)
 
         // 4
         val que4 = Question(
-            4, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_belgium,
-            "Bahamas", "Belgium",
-            "Barbados", "Belize", 2
+            4, "What is the name of this animal?",
+            R.drawable.camel,
+            "Ant", "Camel",
+            "Squirrel", "Sheep", 2
         )
 
         questionsList.add(que4)
 
         // 5
         val que5 = Question(
-            5, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_fiji,
-            "Gabon", "France",
-            "Fiji", "Finland", 3
+            5, "What is the name of this animal?",
+            R.drawable.cat,
+            "Baboon", "Lion",
+            "Cat", "Mongoose", 3
         )
 
         questionsList.add(que5)
@@ -61,49 +61,49 @@ object Constants{
         // 6
         val que6 = Question(
             6, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_germany,
-            "Germany", "Georgia",
-            "Greece", "none of these", 1
+            R.drawable.deer,
+            "Deer", "Goat",
+            "Elephant", "Giraffe", 1
         )
 
         questionsList.add(que6)
 
         // 7
         val que7 = Question(
-            7, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_denmark,
-            "Dominica", "Egypt",
-            "Denmark", "Ethiopia", 3
+            7, "What is the name of this animal?",
+            R.drawable.dog,
+            "Cat", "Tiger",
+            "Dog", "Panda", 3
         )
 
         questionsList.add(que7)
 
         // 8
         val que8 = Question(
-            8, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_india,
-            "Ireland", "Iran",
-            "Hungary", "India", 4
+            8, "What is the name of this mammal?",
+            R.drawable.dolphin,
+            "Man", "Aquaman",
+            "Whale", "Dolphin", 4
         )
 
         questionsList.add(que8)
 
         // 9
         val que9 = Question(
-            9, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_new_zealand,
-            "Australia", "New Zealand",
-            "Tuvalu", "United States of America", 2
+            9, "What is the name of this animal?",
+            R.drawable.donkey,
+            "Camel", "Donkey",
+            "Lama", "Horse", 2
         )
 
         questionsList.add(que9)
 
         // 10
         val que10 = Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
+            10, "What is the name of this bird?",
+            R.drawable.duck,
+            "Duck", "Peacock",
+            "Crow", "Eagle", 1
         )
 
         questionsList.add(que10)
